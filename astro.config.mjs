a072df63cd5651effdf48d268b98fab54029a196
reinaldoStephens <reinaldo9519@gmail.com>
2026-01-08 23:04:37 -0600

    vercel ignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-// @ts-check
+
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  // Cambiamos a 'server' para asegurar que se genere el servidor
   output: 'server',
+  adapter: vercel({
+    // Desactivamos funciones experimentales que podrían mover el entry.mjs de lugar
+    webAnalytics: { enabled: false },
+    isr: false 
+  }),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
